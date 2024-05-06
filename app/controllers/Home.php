8cf9950e5316f3aee537e61a7fbdb39285a19256
Fatimah Azzaroh <116473121+fatimahazzaroh@users.noreply.github.com>
2024-05-07 05:43:51 +0700

    controller, method, params routing

---
+++ app/controllers/Home.php
@@ -0,0 +1,7 @@
+<?php
+
+class Home {
+    public function index() {
+        echo 'Home/index'; // Memanggil function index pada kelas home
+    }
+}
--- app/core/App.php
+++ app/core/App.php
@@ -1,9 +1,38 @@
 <?php
 
 class App {
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct() {
         $url = $this->parseURL();
-        var_dump($url);
+        
+        // Controller
+        // Mengecek apakah ada nama file php di controllers yang sesuai dengan keyword pertama pada url
+        if ( file_exists('../app/controllers' .$url[0] . '.php')) {
+            $this->controller = $url[0]; // Menjadikan url[0] sebagai controller
+            unset($url[0]);
+        };
+
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        // Method
+        if (isset($url[1])){
+            if(method_exists($this->controller, $url[1])){
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        };
+
+        // Params
+        if(!empty($url)) {
+            $this->params = array_values($url);
+        }
+
+        // Jalankan controller & method, serta kirim params jka ada
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     // Ambil url diisi dengan url
